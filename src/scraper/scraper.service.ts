4b698180c0748d4c4d2887daaf00330eee6bff4a
nundung <jjy809123@naver.com>
2024-07-13 21:13:28 +0900

    Refactor: 데이터 추출 방식 간결화

--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -1,7 +1,5 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { firstValueFrom, lastValueFrom } from 'rxjs';
-import { takeCoverage } from 'v8';
 
 const puppeteer = require('puppeteer');
 
@@ -9,58 +7,39 @@ const puppeteer = require('puppeteer');
 export class ScraperService {
   constructor(private readonly httpService: HttpService) {}
 
-  async scrapeMostPlayedCharts() {
-    // Puppeteer 브라우저 실행
+  async scrapMostPlayedCharts() {
     const browser = await puppeteer.launch({
-      headless: false,
+      // headless: false,
     });
     const page = await browser.newPage();
 
-    await page.goto('https://store.steampowered.com/charts/mostplayed', {});
+    try {
+      await page.goto('https://store.steampowered.com/charts/mostplayed', {});
 
-    const tableSelector =
-      '#page_root > div:nth-child(5) > div > div > div > div._3sJkwsBQuiAc_i3VOWX4tv > table';
-    await page.waitForSelector(tableSelector);
+      const tableSelector =
+        '#page_root > div:nth-child(5) > div > div > div > div._3sJkwsBQuiAc_i3VOWX4tv > table';
+      await page.waitForSelector(tableSelector);
 
-    // 각 행의 데이터를 추출
-    const games = await page.evaluate((tableSelector) => {
-      const rows = document.querySelectorAll(`${tableSelector} > tbody > tr`);
-      const gamesList = [];
-
-      rows.forEach((row) => {
-        const blankElement = row.querySelector('td:nth-child(1)');
-        const rankElement = row.querySelector('td:nth-child(2)');
-        const nameElement = row.querySelector('td:nth-child(3)');
-        const priceElement = row.querySelector('td:nth-child(4)');
-        const currentPlayersElement = row.querySelector('td:nth-child(5)');
-        const peakPlayersElement = row.querySelector('td:nth-child(6)');
-
-        const blank = blankElement ? blankElement.textContent.trim() : '';
-        const rank = rankElement ? rankElement.textContent.trim() : '';
-        const name = nameElement ? nameElement.textContent.trim() : '';
-        const price = priceElement ? priceElement.textContent.trim() : '';
-        const currentPlayers = currentPlayersElement
-          ? currentPlayersElement.textContent.trim().replace(/,/g, '')
-          : '';
-        const peakPlayers = peakPlayersElement
-          ? peakPlayersElement.textContent.trim().replace(/,/g, '')
-          : '';
-
-        gamesList.push({
-          blank,
-          rank,
-          name,
-          price,
-          currentPlayers,
-          peakPlayers,
+      // 각 행의 데이터를 추출
+      const games = await page.evaluate((tableSelector) => {
+        const rows = document.querySelectorAll(`${tableSelector} > tbody > tr`);
+        return Array.from(rows).map((row) => {
+          const columns = row.querySelectorAll('td');
+          return {
+            rank: columns[1].innerText.trim(),
+            name: columns[2].innerText.trim(),
+            price: columns[3].innerText.trim(),
+            currentPlayers: columns[4].innerText.trim(),
+            peakPlayers: columns[5].innerText.trim(),
+          };
         });
-      });
+      }, tableSelector);
 
-      return gamesList;
-    }, tableSelector);
-
-    console.log(games);
-
-    await browser.close();
+      console.log(games);
+    } catch (error) {
+      console.error('Error during scraping:', error);
+    } finally {
+      await browser.close();
+    }
   }
 }
