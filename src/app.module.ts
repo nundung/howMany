8f63c2742342131d7d00d4598cb2a36c8b48f7ac
nundung <jjy809123@naver.com>
2024-07-14 15:03:03 +0900

    Feat: scheduleModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,15 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { StaticModule } from './static/static.module';
 import { ScraperModule } from './scraper/scraper.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
-@Module({ imports: [HttpModule, StaticModule, ScraperModule] })
+@Module({
+  imports: [
+    ScheduleModule.forRoot(),
+    ,
+    HttpModule,
+    StaticModule,
+    ScraperModule,
+  ],
+})
 export class AppModule {}
