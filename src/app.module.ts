326973679387ac8f202289bcdef5b1e1736bc1a3
nundung <jjy809123@naver.com>
2024-07-21 19:20:53 +0900

    Feat: appModule에서 MbtiModule import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { HttpModule } from '@nestjs/axios';
 import { StaticModule } from './static/static.module';
 import { ScraperModule } from './scraper/scraper.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { MbtiModule } from './mbti/mbti.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), HttpModule, StaticModule, ScraperModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    HttpModule,
+    StaticModule,
+    ScraperModule,
+    MbtiModule,
+  ],
 })
 export class AppModule {}
