79fef68730e836251acc5e4218472abd657b4a85
nundung <jjy809123@naver.com>
2024-07-29 21:12:05 +0900

    Feat: swagger문서 추가

--- src/mbti/mbti.controller.ts
+++ src/mbti/mbti.controller.ts
@@ -1,18 +1,68 @@
 import {
   Body,
-  Controller,
-  HttpException,
-  HttpStatus,
   Post,
+  Controller,
+  HttpStatus,
+  HttpException,
 } from '@nestjs/common';
 import { MbtiService } from './mbti.service';
 import { AnswersDto } from './dto/answers.dto';
+import {
+  ApiBody,
+  ApiTags,
+  ApiOperation,
+  ApiOkResponse,
+  ApiBadRequestResponse,
+} from '@nestjs/swagger';
 
 @Controller('mbti')
+@ApiTags('mbti API')
 export class MbtiController {
   constructor(private readonly mbtiService: MbtiService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'mbti API',
+    description: '사용자의 mbti 답변결과를 필터링',
+  })
+  @ApiBadRequestResponse({
+    description: 'Invalid request format',
+  })
+  @ApiOkResponse({
+    //영어로 수정하기
+    description: 'OK',
+    schema: {
+      type: 'object',
+      properties: {
+        mbtiType: { type: 'string', example: 'INTJ' }, // MBTI 결과를 문자열로 반환
+        scores: {
+          type: 'object',
+          properties: {
+            I: { type: 'number', example: 2 },
+            E: { type: 'number', example: 1 },
+            N: { type: 'number', example: 4 },
+            S: { type: 'number', example: 2 },
+            T: { type: 'number', example: 4 },
+            F: { type: 'number', example: 0 },
+            J: { type: 'number', example: 5 },
+            P: { type: 'number', example: 2 },
+          },
+        },
+      },
+    },
+  })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        answers: {
+          type: 'array',
+          items: { type: 'number' },
+          example: [1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1], // 사용자 답변 예시
+        },
+      },
+    },
+  })
   async analyzeAnswers(@Body() answersDto: AnswersDto) {
     const { answers } = answersDto;
 
--- src/scraper/scraper.controller.ts
+++ src/scraper/scraper.controller.ts
@@ -1,17 +1,86 @@
-import { Controller, Get, Query } from '@nestjs/common';
 import { ScraperService } from './scraper.service';
-import { query } from 'express';
+import { Controller, Get, Query } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiOkResponse,
+  ApiBadRequestResponse,
+  ApiQuery,
+} from '@nestjs/swagger';
 
 @Controller('scraper')
+@ApiTags('scraper API')
 export class ScraperController {
   constructor(private readonly scraperService: ScraperService) {}
 
+  //당일최다접속게임
+  @ApiOperation({
+    summary: 'scrapMostPlayed API',
+    description: 'MostPlayed 차트를 스크랩해오는 api',
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request',
+  })
+  @ApiOkResponse({
+    description: 'OK',
+    schema: {
+      type: 'object',
+      properties: {
+        charts: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              rank: { type: 'number', example: 1 },
+              name: { type: 'string', example: 'Counter-Strike 2' },
+              price: { type: 'string', example: 'Free To Play' },
+              currentPlayers: { type: 'string', example: '867,766' },
+              peakPlayers: { type: 'string', example: '1,313,419' },
+            },
+          },
+        },
+      },
+    },
+  })
   @Get('mostplayed')
   async scrapMostPlayed() {
     const charts = await this.scraperService.scrapMostPlayedCharts();
     return charts;
   }
 
+  //topseller게임
+  @ApiOperation({
+    summary: 'scrapTopSeller API',
+    description: 'TopSeller 차트를 스크랩해오는 api',
+  })
+  @ApiBadRequestResponse({ description: 'Invalid request parameters' })
+  @ApiOkResponse({
+    description: 'OK',
+    schema: {
+      type: 'object',
+      properties: {
+        charts: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              rank: { type: 'number', example: 9 },
+              name: { type: 'string', example: 'Dead by Daylight' },
+              price: { type: 'string', example: '-60%\n₩ 21,500\n₩ 8,600' },
+              change: { type: 'string', example: '▲ 17' },
+              weeks: { type: 'string', example: '376' },
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiQuery({
+    name: 'region',
+    description: 'Top Seller 차트를 가져올 지역 (예: KR, US, JP, CN)',
+    required: true,
+    type: 'string',
+  })
   @Get('topseller')
   async scrapTopSeller(@Query('region') region: string) {
     const charts = await this.scraperService.scrapTopSellerCharts(region);
