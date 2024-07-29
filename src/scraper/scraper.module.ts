54be9e959eff30180c983f817e612b6deadc4c19
nundung <jjy809123@naver.com>
2024-07-29 16:53:53 +0900

    Feat: scrap한 차트데이터를 caching 하는 기능 추가

--- src/scraper/scraper.module.ts
+++ src/scraper/scraper.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ScraperService } from './scraper.service';
 import { ScraperController } from './scraper.controller';
 import { HttpModule } from '@nestjs/axios';
+import { CacheMoudle } from 'src/common/cache/cache.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CacheMoudle],
   controllers: [ScraperController],
   providers: [ScraperService],
 })
