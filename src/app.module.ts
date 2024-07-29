53bb2d705a09a18a205c7738240e70b47d6b9853
nundung <jjy809123@naver.com>
2024-07-29 16:56:16 +0900

    Feat: app.module에cachemodule import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { StaticModule } from './static/static.module';
 import { ScraperModule } from './scraper/scraper.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { MbtiModule } from './mbti/mbti.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
+    CacheModule.register({
+      store: 'memory', // 기본 메모리 캐시 스토어
+      max: 1000, // 최대 캐시 항목 수
+      ttl: 600, // 기본 TTL (초)
+      isGlobal: true,
+    }),
     ScheduleModule.forRoot(),
     HttpModule,
     StaticModule,
