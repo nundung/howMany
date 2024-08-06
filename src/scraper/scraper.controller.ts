258c62d272cb5878e7b4cea82318efed86a094d0
nundung <jjy809123@naver.com>
2024-08-06 20:00:13 +0900

    Feat: swagger 문서 추가

--- src/scraper/scraper.controller.ts
+++ src/scraper/scraper.controller.ts
@@ -36,6 +36,11 @@ export class ScraperController {
               price: { type: 'string', example: 'Free To Play' },
               currentPlayers: { type: 'string', example: '867,766' },
               peakPlayers: { type: 'string', example: '1,313,419' },
+              imageUrl: {
+                type: 'string',
+                example:
+                  'https://shared.akamai.steamstatic.com//store_item_assets/steam/apps/730/capsule_231x87.jpg?t=1719426374',
+              },
             },
           },
         },
@@ -69,6 +74,11 @@ export class ScraperController {
               price: { type: 'string', example: '-60%\n₩ 21,500\n₩ 8,600' },
               change: { type: 'string', example: '▲ 17' },
               weeks: { type: 'string', example: '376' },
+              imageUrl: {
+                type: 'string',
+                example:
+                  'https://shared.akamai.steamstatic.com//store_item_assets/steam/apps/381210/capsule_231x87.jpg?t=1721927210',
+              },
             },
           },
         },
